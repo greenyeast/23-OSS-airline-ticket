dd01a03f097859d1f1229a66db0c66174d08cfd6
oldprize47 <qkrwkdrns01@handong.ac.kr>
2023-05-16 19:06:01 +0900

    login기능 수정했습니다.

--- logIn.c
+++ logIn.c
@@ -3,73 +3,138 @@
 #include <stdlib.h>
 
 typedef struct{
-    char userId[20];
-    char pw[20];
-    int session; //�������� ��� 1, �̿����� ��� 0.
+    char name[20];
+    int age;
+    char userId[50];
+    char pw[50];
 } User;
 
-int checkID(char ID[], char pw[], int *session);    // ���̵�,��й�ȣ Ȯ��.
-void new_member();                                  // ȸ������.
-// int showLoginMenu();
+int checkAdminID(char ID[], char pw[]);     // 관리자 아이디,비밀번호 확인.
+int checkUserID(char ID[], char pw[]);      // 유저
+void new_member();                                  // 회원가입.
+int showStartMenu();
+int showLoginMenu();
 
 int main(){
-    char ID[20];
-    char pw[20];
+    char ID[50];
+    char pw[50];
     int session = 0;
-    // new_member();
-    // strcpy(u->userId, "qkrwks01");
-    // strcpy(u->pw, "12367");
+
     while(1){
-        printf("ID : ");
-        scanf("%s", ID);
-        getchar();
-        printf("PW : ");
-        scanf("%s", pw);
-        getchar();
-        if(checkID(ID,pw,&session)) break;
+        session = showStartMenu();
+        if(session == 1){
+            printf("사용자 모드로 시작합니다.\n");
+            break;
+        }else if(session == 2){
+            printf("관리자 모드로 시작합니다.\n");
+            break;
+        }else{
+            printf("잘못 입력하셨습니다.\n");
+        }
     }
 
-    if(session){
-        printf("������ �Դϴ�.");
-    }else{
-        printf("�̿��� �Դϴ�.");
+    while(1){
+        int loginMode = 0;
+        if(session == 1){
+            loginMode = showLoginMenu();
+        }
+        if(loginMode == 1){
+            new_member();
+        }else if(loginMode == 2 || session == 2){
+            //로그인
+            while(1){
+                printf("ID : ");
+                scanf("%s", ID);
+                getchar();
+                printf("PW : ");
+                scanf("%s", pw);
+                getchar();
+                if(session == 2){
+                    if(checkAdminID(ID,pw)) break;
+                }else{
+                    if(checkUserID(ID,pw)) break;
+                }
+                
+            }
+            break;
+        }else{
+            printf("잘못 입력하셨습니다.\n");
+        }
     }
+
     return 0;
 }
-// int showLoginMenu(){
-//     int menu;
-//     printf("\n***************HanAir Ticket System ***************\n");
-//     printf("1. �����\n");
-//     printf("2. ������\n");
-//     printf("********************************************************\n");
-//     printf("\n");
-//     printf("=> ���ϴ� �޴���? ");
 
-//     scanf(" %d", &menu);
-//     getchar();
-//     return menu;
-// }
+int showLoginMenu(){
+    int menu;
+    printf("\n***************HanAir Ticket System ***************\n");
+    printf("1. HanAir를 처음 사용하십니까? 회원가입\n");
+    printf("2. HanAir를 사용한 적이 있나요? 로그인\n");
+    printf("********************************************************\n");
+    printf("\n");
+    printf("=> 원하는 메뉴는? ");
 
-int checkID(char ID[], char pw[], int *admin){
+    scanf(" %d", &menu);
+    getchar();
+    return menu;
+}
+int showStartMenu(){
+    int menu;
+    printf("\n***************HanAir Ticket System ***************\n");
+    printf("1. 사용자\n");
+    printf("2. 관리자\n");
+    printf("********************************************************\n");
+    printf("\n");
+    printf("=> 원하는 메뉴는? ");
+
+    scanf(" %d", &menu);
+    getchar();
+    return menu;
+}
+
+int checkUserID(char ID[], char pw[]){
+
+    
     FILE * file = fopen("userList.txt", "r");
     char line[100];
     while(fgets(line,sizeof(line),file)){
         User *u= (User *)malloc(sizeof(User));
-        sscanf(line, "%s %s %d\n", u->userId, u->pw, &u->session);
+        sscanf(line, "%s %d %s %s\n", u->name, &u->age, u->userId, u->pw);
         if(!strcmp(ID,u->userId)){
             if(strcmp(pw,u->pw)){
-                printf("��й�ȣ�� Ʋ�Ƚ��ϴ�.\n");
+                printf("비밀번호가 틀렸습니다.\n");
                 return 0;
             }else{
-                printf("�α��� ����\n");
-                *admin = u->session;          //�������� ��� 1, ������ ��� 0
+                printf("로그인 성공\n");
                 return 1;
             }
         }
     }
 
     fclose(file);
-    printf("��ġ�ϴ� ID�� �����ϴ�.\n");
+    printf("일치하는 ID가 없습니다.\n");
+    return 0;
+}
+
+int checkAdminID(char ID[], char pw[]){
+    FILE * file = fopen("adminList.txt", "r");
+    char line[100];
+    while(fgets(line,sizeof(line),file)){
+        User *u= (User *)malloc(sizeof(User));
+        sscanf(line, "%s %d %s %s\n", u->name, &u->age, u->userId, u->pw);
+        if(!strcmp(ID,u->userId)){
+            if(strcmp(pw,u->pw)){
+                printf("비밀번호가 틀렸습니다.\n");
+                return 0;
+            }else{
+                printf("로그인 성공\n");
+                return 1;
+            }
+        }
+    }
+
+    fclose(file);
+    printf("일치하는 ID가 없습니다.\n");
     return 0;
 }
 
@@ -77,26 +142,36 @@ void new_member(){
     User *u= (User *)malloc(sizeof(User));
     FILE * file = fopen("userList.txt", "a+");
     char line[100];
+
+    printf("\n*************** HanAir Ticket System ***************\n");
+    printf("이름은? ");
+    fgets(u->name, 20, stdin);
+    u->name[strlen(u->name) - 1] = '\0';
+    
+    printf("나이는? ");
+    scanf("%d", &u->age);
+    getchar();
+
     while(1){
         int plag = 0;
-        printf("���ο� ID�� �Է��ϼ��� (3���� �̻�): ");
-        fgets(u->userId, 20, stdin);
+        printf("새로운 ID를 입력하세요 (3글자 이상): ");
+        fgets(u->userId, 50, stdin);
         u->userId[strlen(u->userId) - 1] = '\0';
         if(strlen(u->userId) < 3){
-            printf("3���� �̻� �Է����ּ���.\n");
+            printf("3글자 이상 입력해주세요.\n");
             continue;
         }
 
         while(fgets(line,sizeof(line),file)){
             User *check= (User *)malloc(sizeof(User));
-            sscanf(line, "%s %s %d\n", check->userId, check->pw, &check->session);
+            sscanf(line, "%s %d %s %s\n", check->name, &check->age, check->userId, check->pw);
             if(!strcmp(u->userId, check->userId)){
                 plag = 1;
                 break;
             }
         }
         if(plag){
-            printf("������ ID�� �����մϴ�.\n");
+            printf("동일한 ID가 존재합니다.\n");
             continue;
         }else{
             break;
@@ -104,22 +179,21 @@ void new_member(){
     }
 
     while(1){
-        printf("pw�� �Է��ϼ��� (3���� �̻�): ");
-        fgets(u->pw, 20, stdin);
+        printf("pw를 입력하세요 (3글자 이상): ");
+        fgets(u->pw, 50, stdin);
         u->pw[strlen(u->pw) - 1] = '\0';
 
         if(strlen(u->pw) < 3){
-            printf("3���� �̻� �Է����ּ���.\n");
+            printf("3글자 이상 입력해주세요.\n");
             continue;
         }else{
             break;
         }
     }
 
-    u->session = 0;
-
-    fprintf(file,"%s %s %d\n", u->userId, u->pw, u->session);
-    printf("ȸ�������� �Ϸ�Ǿ����ϴ�.\n");
+    fprintf(file,"%s %d %s %s\n", u->name, u->age, u->userId, u->pw);
+    printf("********************************************************\n");
+    printf("=> 사용자 계정이 생성되었습니다. 로그인해주세요.\n");
     fclose(file);
     return;
 }
