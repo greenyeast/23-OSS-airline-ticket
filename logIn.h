1593202e61b20720ab41e9eb451e5c3a486d0275
greenyeast <djaks65@gmail.com>
2023-05-22 20:56:01 +0900

    Changes in ticket.c

--- logIn.h
+++ logIn.h
@@ -7,9 +7,9 @@ typedef struct{
     int age;
 } User;
 
-int checkAdminID(char ID[], char pw[]);     // 관리자 아이디,비밀번호 확인.
+int checkAdminID(char ID[], char pw[]);     // 관리자 아이디,비밀번호 확인
 int checkUserID(char ID[], char pw[]);      // 유저
-void new_member();                                  // 회원가입.
-int showStartMenu();
-int showLoginMenu();
+void new_member();                          // 회원가입
+int showStartMenu();                        // 사용자, 관리자 시작 화면 
+int showLoginMenu();                        // 로그인 화면 
 #endif
